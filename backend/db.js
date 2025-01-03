9301a590af851553ca4461904f1049d8a8070124
Maxwell Wijaya <maxwellwijaya00@gmail.com>
2025-01-03 14:05:35 -0800

    database update 2

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@ const mysql = require('mysql2/promise');
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: '46.202.197.83', // Your database host      
+    host: '46.202.197.83', // Your database host 
+    port: 3306,     
     user: 'u513798782_relife',            
     password: '>9jS+B|z',    // Your MySQL password
     database: 'u513798782_relife',      // Your database name
@@ -20,4 +21,4 @@ pool.getConnection((err, connection) => {
     connection.release();
 });
 
-// module.exports = pool;
+module.exports = pool;
